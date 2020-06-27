e879aa6063bd1046935e78ed0f56b7a3845d7dc3
Ganzeroar <ganzeroar@localhost.localdomain>
2020-06-27 18:00:22 +0300

    create base view

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,22 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
     '''test home page'''
 
     def test_root_url_resolves_to_home_page_view(self):
-        '''beginning test'''
+        '''test root url resolve to home page view'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''test home page returns correct html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
