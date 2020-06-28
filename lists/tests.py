569622b7858c42f2bfef4d5b5ebec352f215ecf9
Ganzeroar <ganzeroar@localhost.localdomain>
2020-06-28 20:34:12 +0300

    refactorized tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,14 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
     '''test home page'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''test root url resolve to home page view'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         '''test home page returns correct html'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
